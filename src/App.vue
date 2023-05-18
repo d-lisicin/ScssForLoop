65543402ad87c05e0e1380fdfe7507a8fe360bf0
d.lisicin <d.lisicin@jetmail.cc>
2023-05-18 12:51:01 +0300

    Sample scss loop

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <navigate />
+</template>
+
+<script>
+
+import Navigate from '@/components/navigate'
+export default {
+  name: 'App',
+  components: {
+    Navigate
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "assets/scss/main.scss";
+</style>
---
+++ src/components/navigate/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="menu">
+    <div class="menu__active">Пункт 1</div>
+    <ul class="menu__list">
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 1</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 2</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 3</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 4</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 5</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 6</a>
+      </li>
+      <li class="menu__item">
+        <a class="menu__link" href="">Пункт 7</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navigate'
+}
+</script>
+
+<style src="./style.scss" lang="scss"></style>
